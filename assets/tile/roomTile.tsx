e534e4b5783616c269e27bc67abbab8304ef458f
SummerXLan <3163964969@qq.com>
2022-10-15 12:22:06 +0800

    第一次提交

---
+++ assets/tile/roomTile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="roomTile" tilewidth="16" tileheight="16" spacing="8" tilecount="240" columns="12">
+ <image source="roomTile.png" width="280" height="472"/>
+</tileset>
